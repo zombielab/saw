df16c7fd76dae4b7ffb031718f7f538e52bcc8d3
Huseyin DOGAN <dogan.h@expoweb.fr>
2016-08-15 22:39:09 +0200

    Small fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // compiled dir/files are meant to be removed as soon as node handle async/await and es2015 modules
 // until then we'll do this way.
-var App = new require("./app/src-compiled/app");
+var App = require("./app/src-compiled/app");
 
 var app = new (App.default)();
 
